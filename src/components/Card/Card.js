763e1256bb11684e172d5c16729e960c57040ebb
Mike Dwyer <brianmichaeldwyer@gmail.com>
2019-06-17 14:17:27 -0700

    view all cards

---
+++ src/components/Card/Card.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class Card extends React.Component {
+
+  componentDidMount() {
+    console.log(this.props.name)
+  }
+
+  render() {
+    return (
+      <div>
+        <p>Name: {this.props.name}</p>
+        <img src={this.props.image} alt={this.props.name}/>
+        <p>Description: {this.props.description}</p>
+        <p>Factoid: {this.props.factoid}</p>
+      </div>
+    );
+  }
+}
+
+export default Card;
--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import cardService from '../../utils/cardService';
 
+import Card from '../Card/Card';
 
 class Cards extends React.Component {
+
   state = {
     cards: [],
   };
@@ -17,6 +19,15 @@ class Cards extends React.Component {
   render() {
     return (
       <div>
+        {!this.state.cards[0] && <p>Loading...</p>}
+        {this.state.cards.map(card => (
+          <Card 
+          name={card.name}
+          image={card.image}
+          description={card.description}
+          factoid={card.factoid}
+          />
+        ))}
       </div>
     );
   }
